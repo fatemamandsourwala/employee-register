d560bd1c84cec7fb359af9d6f668a5d6f8a939fb
Fatema Mandsourwala <f.mandsourwala4993@gmail.com>
2020-05-11 22:34:36 +1000

    Updates

---
+++ precache-manifest.44c220b3eaf12e3d5fd744da16ac7644.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08cf2393aee102bd23e117290002d1f4",
+    "url": "/employee-register/index.html"
+  },
+  {
+    "revision": "658cca9eecdb86f8f839",
+    "url": "/employee-register/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "316e011bf706ea1bc98d",
+    "url": "/employee-register/static/css/main.7aed7741.chunk.css"
+  },
+  {
+    "revision": "658cca9eecdb86f8f839",
+    "url": "/employee-register/static/js/2.1326a0e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employee-register/static/js/2.1326a0e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "316e011bf706ea1bc98d",
+    "url": "/employee-register/static/js/main.a68969a8.chunk.js"
+  },
+  {
+    "revision": "55cdcc3237be6773b82e",
+    "url": "/employee-register/static/js/runtime-main.73ad57c7.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/employee-register/static/media/SEGA.12a3db8b.woff"
+  }
+]);
